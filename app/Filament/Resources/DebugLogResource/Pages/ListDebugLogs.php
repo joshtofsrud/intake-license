610cc046bd26ca3a6a868c0775c00571d2d84f8d
joshtofsrud <joshtofsrud@gmail.com>
2026-04-17 01:06:11 -0700

    Update ListDebugLogs.php

--- app/Filament/Resources/DebugLogResource/Pages/ListDebugLogs.php
+++ app/Filament/Resources/DebugLogResource/Pages/ListDebugLogs.php
@@ -5,10 +5,7 @@ namespace App\Filament\Resources\DebugLogResource\Pages;
 use App\Filament\Resources\DebugLogResource;
 use App\Filament\Widgets\DebugLogHeaderStats;
 use App\Filament\Widgets\DebugLogErrorsChart;
-use App\Models\DebugLog;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Resources\Pages\ListRecords\Tab;
-use Illuminate\Database\Eloquent\Builder;
 
 class ListDebugLogs extends ListRecords
 {
@@ -19,39 +16,6 @@ class ListDebugLogs extends ListRecords
         return [
             DebugLogHeaderStats::class,
             DebugLogErrorsChart::class,
-        ];
-    }
-
-    public function getTabs(): array
-    {
-        return [
-            'all' => Tab::make('All'),
-
-            'errors' => Tab::make('Errors')
-                ->modifyQueryUsing(fn (Builder $q) => $q
-                    ->where('channel', 'error')
-                    ->where('is_resolved', false)),
-
-            'requests' => Tab::make('Requests')
-                ->modifyQueryUsing(fn (Builder $q) => $q->where('channel', 'request')),
-
-            'mail' => Tab::make('Mail')
-                ->modifyQueryUsing(fn (Builder $q) => $q->whereIn('channel', ['mail', 'sms'])),
-
-            'auth' => Tab::make('Auth')
-                ->modifyQueryUsing(fn (Builder $q) => $q->where('channel', 'auth')),
-
-            'audit' => Tab::make('Audit')
-                ->modifyQueryUsing(fn (Builder $q) => $q->where('channel', 'audit')),
-
-            'jobs' => Tab::make('Jobs')
-                ->modifyQueryUsing(fn (Builder $q) => $q->where('channel', 'job')),
-
-            'webhooks' => Tab::make('Webhooks')
-                ->modifyQueryUsing(fn (Builder $q) => $q->where('channel', 'webhook')),
-
-            'impersonation' => Tab::make('Impersonation')
-                ->modifyQueryUsing(fn (Builder $q) => $q->where('channel', 'impersonation')),
         ];
     }
 }
