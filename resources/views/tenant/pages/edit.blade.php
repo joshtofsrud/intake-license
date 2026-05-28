bda7063df0f11e2bb9ba7a4786a0a82777f29174
joshtofsrud <joshtofsrud@gmail.com>
2026-05-27 23:01:00 -0700

    patch-158-G30a: add pricing_table to tenant gallery

--- resources/views/tenant/pages/edit.blade.php
+++ resources/views/tenant/pages/edit.blade.php
@@ -1289,7 +1289,7 @@
           @php
             $allowed = $isMarketing
               ? ['nav','hero','text_image','cta_banner','image_gallery','contact_form','feature_grid','step_timeline','faq_accordion','footer','pricing_table','testimonial_carousel','logo_bar','stats_row','comparison_table','industry_pack_showcase']
-              : ['nav','hero','text_image','cta_banner','image_gallery','contact_form','booking_embed','classes_embed','feature_grid','step_timeline','faq_accordion','footer','testimonial_carousel','logo_bar','stats_row'];
+              : ['nav','hero','text_image','cta_banner','image_gallery','contact_form','booking_embed','classes_embed','feature_grid','step_timeline','faq_accordion','footer','testimonial_carousel','logo_bar','stats_row','pricing_table'];
           @endphp
 
           <div class="pb2-gallery">
